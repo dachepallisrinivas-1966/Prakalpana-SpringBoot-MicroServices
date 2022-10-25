029930c8bc64f40f6426a2f9322a5185895eb538
Srinivas <dachepallisrinivas@gmail.com>
2022-10-25 09:07:02 +0530

    day7

--- SpringWorkspace/spring-core-javabased-configuration-demo/src/main/java/com/prak/config/MyBeanConfig.java
+++ SpringWorkspace/spring-core-javabased-configuration-demo/src/main/java/com/prak/config/MyBeanConfig.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.prak")
+@PropertySource("classpath:application.properties")
 public class MyBeanConfig {
     @Bean
     public Scanner kbinput() {
--- SpringWorkspace/spring-core-javabased-configuration-demo/src/main/java/com/prak/ui/HomeUI.java
+++ SpringWorkspace/spring-core-javabased-configuration-demo/src/main/java/com/prak/ui/HomeUI.java
@@ -3,23 +3,37 @@ package com.prak.ui;
 import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.prak.service.GreetService;
 
 @Component
 public class HomeUI {
+	
+	@Value("${app.title:Spring First Application}")
+	private String appTitle;
+	
 	@Autowired
 	private Scanner scan;
 	
 	@Autowired
+	@Qualifier("greetServiceTimeBasedImpl")
 	private GreetService gs;
 	
+	@Autowired
+	@Qualifier("greetServiceDefaultImpl")
+	private GreetService gs2;
+	
 	public void run() {
+		System.out.println(appTitle);
+		System.out.println("---------------------------------------------------------");
 		System.out.println("Enter user name: ");
 		String userName = scan.nextLine();
 		
 		System.out.println(gs.greet(userName));
+		System.out.println(gs2.greet(userName));
 	}
 }
 
